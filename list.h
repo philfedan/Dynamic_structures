bf219df7bf8c7b5355971c478ce79891d0437153
philfedan <philfedan@gmail.com>
2015-12-19 16:54:51 +0300

    Create list.h

---
+++ list.h
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+struct node
+{
+	int d;
+	node *next;
+};
+
+node *first(int d);
+void add(node **pend, int d);
+int del(node **pbeg);
+void print(node *pbeg);
+bool is_Empty(node *pbeg);
+node *find(node *const pbeg, int d);
+bool del_key(node **pbeg, int key);
+void insert(node *const pbeg, int key, int d);
+void insert_before(node **pbeg, int key, int d);
+void sort(node *pbeg);
